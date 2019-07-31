5a582dfffbe8311fc5eb9a51cad645bfc1f1a557
Josh Morris <jmo0624@gmail.com>
2019-07-31 01:03:59 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,28 +1,27 @@
 class Doctor
-  
-  attr_accessor :name
-  
+
+   attr_accessor :name
   @@all = []
-  
-  def self.all
-    @@all
-  end
-  
-  def initialize(name)
+
+   def initialize(name)
     @name = name
     @@all << self
   end
-  
-  def new_appointment(patient, date)
-    Appointment.new(patient, date, self)
-  end
-  
-  def appointments
+
+   def appointments
     Appointment.all.select {|appointment| appointment.doctor == self}
   end
-  
-  def patients
+
+   def patients
     appointments.map {|appointment| appointment.patient}
   end
-  
-end
+
+   def self.all
+    @@all
+  end
+
+   def new_appointment(patient, date)
+    Appointment.new(patient, date, self)
+  end
+
+ end 
