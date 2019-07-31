80a300ce9871f7c8be5257d911e9df2f1ff6a070
Josh Morris <jmo0624@gmail.com>
2019-07-31 01:17:57 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,22 +1,22 @@
 class Patient
 
-   attr_reader :name, :appointment, :doctor
+  attr_reader :name, :appointment, :doctor
 
-   def initialize(name)
+  def initialize(name)
     @name = name
   end
 
-   def new_appointment(doctor, date)
+  def new_appointment(doctor, date)
     Appointment.new(self, doctor, date)
   end
 
-   def appointments
+  def appointments
     Appointment.all.select do |appointment|
       appointment.patient == self
     end
   end
 
-   def doctors
+  def doctors
     Appointment.all.map do |appointment|
       appointment.doctor
     end
