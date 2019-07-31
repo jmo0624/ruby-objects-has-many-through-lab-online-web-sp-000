6fd7cebe3ecc408cb5634c99f359e23292f14b8b
Josh Morris <jmo0624@gmail.com>
2019-07-31 01:04:30 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,18 +1,17 @@
+
 class Appointment
-  
-  attr_accessor :date, :patient, :doctor
-  
+
+   attr_accessor :date, :patient, :doctor
   @@all = []
-  
-  def self.all
-    @@all
-  end
-  
-  def initialize(patient, date, doctor)
+
+   def initialize(patient, date, doctor)
     @patient = patient
     @date = date
     @doctor = doctor
     @@all << self
   end
-  
-end
+
+   def self.all
+    @@all
+  end
+end 
