8080a19c6ab55562a6dcaa0b699a0746c213e7ca
Josh Morris <jmo0624@gmail.com>
2019-07-31 00:29:34 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,15 @@
 class Artist
   
-  attr_accessor
+  attr_accessor :name, :songs
+  
+  @@all = []
+  
+  def self.all
+    @@all
+  end
+  
+  def intialize(name)
+    @name = name
+    @songs = songs
+    @@all << self
+  end
